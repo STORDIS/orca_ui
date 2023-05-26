a79569812f76e9121224ca05691c2601283cefd2
kamalkrbh <kamal.kr.bh@gmail.com>
2023-05-26 09:33:43 +0200

    moved backend code to a separate repository.

--- device/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Device
-
-class DeviceSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Device 
-        fields = ('pk', 'img_name', 'mgt_intf', 'mgt_ip', 'hwsku', 'mac', 'platform', 'type')
