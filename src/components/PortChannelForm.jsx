c78690d3ed2ec893661c9586bb8e13acef3cc655
vivek-metricdust <vivek.v@metricdust.com>
2024-03-15 12:40:47 +0530

    button disable and dropdown added

--- src/components/PortChannelForm.jsx
+++ src/components/PortChannelForm.jsx
@@ -1,29 +1,54 @@
-import React, { useState } from 'react';
-import './tabbedpane/Form.scss'
+import React, { useEffect, useState } from "react";
+import "./tabbedpane/Form.scss";
+import axios from "axios";
+import { getAllInterfacesOfDeviceURL } from "../backend_rest_urls";
 
-const PortChannelForm = ({ onSubmit, selectedDeviceIp, onCancel }) => {
+const PortChannelForm = ({
+    onSubmit,
+    selectedDeviceIp,
+    onCancel,
+    handelSubmitButton,
+}) => {
+    const [disableSubmit, setDisableSubmit] = useState(handelSubmitButton);
+
     const [formData, setFormData] = useState({
-        mgt_ip: selectedDeviceIp || '',
-        lag_name: '',
-        admin_sts: '',
+        mgt_ip: selectedDeviceIp || "",
+        lag_name: "",
+        admin_sts: "",
         mtu: 9100,
-        members: '',
+        members: "",
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
 
-        if (name === 'mtu' && parseInt(value) < 0) {
+        if (name === "mtu" && parseInt(value) < 0) {
             return;
         }
-        setFormData(prevFormData => ({
+        setFormData((prevFormData) => ({
             ...prevFormData,
-            [name]: value
+            [name]: value,
         }));
+
+        console.log('change',formData);
+
+    };
+
+    const handleDropdownChange = (e) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            members: e.target.value,
+        }));
+        console.log('drop',formData);
     };
 
     const handleSubmit = (e) => {
-        const membersArray = formData.members.split(',').map(member => member.trim()).filter(part => part);
+        setDisableSubmit(true);
+
+        const membersArray = formData.members
+            .split(",")
+            .map((member) => member.trim())
+            .filter((part) => part);
         const dataToSubmit = {
             ...formData,
             members: membersArray,
@@ -32,69 +57,112 @@ const PortChannelForm = ({ onSubmit, selectedDeviceIp, onCancel }) => {
     };
 
     const handleValue = (e) => {
-        console.log('valiue', e)
+        console.log("valiue", e);
         if (!/^PortChannel\d+$/.test(e.target.value)) {
-            alert('Invalid lag_name format. It should follow the pattern "PortChannel..." where "..." is a numeric value.');
+            alert(
+                'Invalid lag_name format. It should follow the pattern "PortChannel..." where "..." is a numeric value.'
+            );
             return;
         }
-    }
+    };
+
+    const [interfaceNames, setInterfaceNames] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(getAllInterfacesOfDeviceURL(selectedDeviceIp))
+            .then((response) => {
+                const fetchedInterfaceNames = response.data.map(
+                    (item) => item.name
+                );
+                setInterfaceNames(fetchedInterfaceNames);
+            })
+            .catch((error) => {
+                console.error("Error fetching interface names", error);
+            });
+    }, []);
 
     return (
-        <div className="form-wrapper">
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit(formData);
-        }} className="port-channel-form">
-            <div className="form-field">
-                <label>Device IP:</label>
-                <span>{selectedDeviceIp}</span>
-            </div>
-            <div className="form-field">
-                <label htmlFor="lag-name">Channel Name:</label>
-                <input
-                    type="text"
-                    name="lag_name"
-                    value={formData.lag_name}
-                    onChange={handleChange}
-                    onBlur={handleValue}
-                />
-            </div>
-            <div className="form-field">
-                <label>Admin Status:</label>
-                <select
-                    name="admin_sts"
-                    value={formData.admin_sts}
-                    onChange={handleChange}
-                >
-                    <option value="up">up</option>
-                    <option value="down">down</option>
-                </select>
-            </div>
-            <div className="form-field">
-                <label>MTU:</label>
-                <input
-                    type="number"
-                    name="mtu"
-                    value={formData.mtu}
-                    onChange={handleChange}
-                />
-            </div>
+        <div className="">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit(formData);
+                }}
+                className="port-channel-form"
+            >
+                <div className="form-field">
+                    <label>Device IP:</label>
+                    <span>{selectedDeviceIp}</span>
+                </div>
+                <div className="form-field">
+                    <label htmlFor="lag-name">Channel Name:</label>
+                    <input
+                        type="text"
+                        name="lag_name"
+                        value={formData.lag_name}
+                        onChange={handleChange}
+                        onBlur={handleValue}
+                    />
+                </div>
+                <div className="form-field">
+                    <label>Admin Status:</label>
+                    <select
+                        name="admin_sts"
+                        value={formData.admin_sts}
+                        onChange={handleChange}
+                    >
+                        <option value="up">up</option>
+                        <option value="down">down</option>
+                    </select>
+                </div>
+                <div className="form-field">
+                    <label>MTU:</label>
+                    <input
+                        type="number"
+                        name="mtu"
+                        value={formData.mtu}
+                        onChange={handleChange}
+                    />
+                </div>
 
-            <div className="form-field">
-                <label>Members:</label>
-                <input
-                    type="text"
-                    name="members"
-                    value={formData.members}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="form-field">
-                <input type="submit" value="Submit" />
-                <button type='button' onClick={onCancel}>Cancel</button>
+                <div className="form-field">
+                    <label>Members: </label>
+                    {/* <input
+                        type="text"
+                        name="members"
+                        value={formData.members}
+                        onChange={handleChange}
+                    /> */}
 
-            </div>
-        </form>
+                    <select
+                        id="memberDropdown"
+                        onChange={handleDropdownChange}
+                        value={formData.members}
+                    >
+                        <option value="" disabled>
+                            Select Member Interface
+                        </option>
+                        {interfaceNames.map((val, index) => (
+                            <option key={index} value={val}>
+                                {val}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="">
+                    <button
+                        type="submit"
+                        className="btnStyle"
+                        disabled={disableSubmit}
+                    >
+                        Apply Config
+                    </button>
+                    <button type="button" onClick={onCancel}>
+                        Cancel
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
