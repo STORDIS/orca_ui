25b3f338997c8561e6745c1a8f01ac00def06ba7
vivek-metricdust <vivek.v@metricdust.com>
2024-03-18 16:46:04 +0530

    revert

--- src/components/PortChannelForm.jsx
+++ src/components/PortChannelForm.jsx
@@ -1,32 +1,29 @@
-import React, { useState } from "react";
-import "./tabbedpane/Form.scss";
+import React, { useState } from 'react';
+import './tabbedpane/Form.scss'
 
 const PortChannelForm = ({ onSubmit, selectedDeviceIp, onCancel }) => {
     const [formData, setFormData] = useState({
-        mgt_ip: selectedDeviceIp || "",
-        lag_name: "",
-        admin_sts: "",
+        mgt_ip: selectedDeviceIp || '',
+        lag_name: '',
+        admin_sts: '',
         mtu: 9100,
-        members: "",
+        members: '',
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
 
-        if (name === "mtu" && parseInt(value) < 0) {
+        if (name === 'mtu' && parseInt(value) < 0) {
             return;
         }
-        setFormData((prevFormData) => ({
+        setFormData(prevFormData => ({
             ...prevFormData,
-            [name]: value,
+            [name]: value
         }));
     };
 
     const handleSubmit = (e) => {
-        const membersArray = formData.members
-            .split(",")
-            .map((member) => member.trim())
-            .filter((part) => part);
+        const membersArray = formData.members.split(',').map(member => member.trim()).filter(part => part);
         const dataToSubmit = {
             ...formData,
             members: membersArray,
@@ -35,83 +32,69 @@ const PortChannelForm = ({ onSubmit, selectedDeviceIp, onCancel }) => {
     };
 
     const handleValue = (e) => {
-        console.log("valiue", e);
+        console.log('valiue', e)
         if (!/^PortChannel\d+$/.test(e.target.value)) {
-            alert(
-                'Invalid lag_name format. It should follow the pattern "PortChannel..." where "..." is a numeric value.'
-            );
+            alert('Invalid lag_name format. It should follow the pattern "PortChannel..." where "..." is a numeric value.');
             return;
         }
-    };
+    }
 
     return (
         <div className="form-wrapper">
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    handleSubmit(formData);
-                }}
-                className="port-channel-form"
-            >
-                <div className="form-field">
-                    <label>Device IP:</label>
-                    <span>{selectedDeviceIp}</span>
-                </div>
-                <div className="form-field">
-                    <label htmlFor="lag-name">Channel Name:</label>
-                    <input
-                        type="text"
-                        name="lag_name"
-                        value={formData.lag_name}
-                        onChange={handleChange}
-                        onBlur={handleValue}
-                    />
-                </div>
-                <div className="form-field">
-                    <label>Admin Status:</label>
-                    <select
-                        name="admin_sts"
-                        value={formData.admin_sts}
-                        onChange={handleChange}
-                    >
-                        <option value="up">up</option>
-                        <option value="down">down</option>
-                    </select>
-                </div>
-                <div className="form-field">
-                    <label>MTU:</label>
-                    <input
-                        type="number"
-                        name="mtu"
-                        value={formData.mtu}
-                        onChange={handleChange}
-                    />
-                </div>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit(formData);
+        }} className="port-channel-form">
+            <div className="form-field">
+                <label>Device IP:</label>
+                <span>{selectedDeviceIp}</span>
+            </div>
+            <div className="form-field">
+                <label htmlFor="lag-name">Channel Name:</label>
+                <input
+                    type="text"
+                    name="lag_name"
+                    value={formData.lag_name}
+                    onChange={handleChange}
+                    onBlur={handleValue}
+                />
+            </div>
+            <div className="form-field">
+                <label>Admin Status:</label>
+                <select
+                    name="admin_sts"
+                    value={formData.admin_sts}
+                    onChange={handleChange}
+                >
+                    <option value="up">up</option>
+                    <option value="down">down</option>
+                </select>
+            </div>
+            <div className="form-field">
+                <label>MTU:</label>
+                <input
+                    type="number"
+                    name="mtu"
+                    value={formData.mtu}
+                    onChange={handleChange}
+                />
+            </div>
 
-                <div className="form-field">
-                    <label>Members:</label>
-                    <input
-                        type="text"
-                        name="members"
-                        value={formData.members}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="">
-                    <input
-                        type="submit"
-                        className="btnStyle mr-10"
-                        value="Submit"
-                    />
-                    <button
-                        type="button"
-                        className="btnStyle mr-10"
-                        onClick={onCancel}
-                    >
-                        Cancel
-                    </button>
-                </div>
-            </form>
+            <div className="form-field">
+                <label>Members:</label>
+                <input
+                    type="text"
+                    name="members"
+                    value={formData.members}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-field">
+                <input type="submit" className="btnStyle mr-10" value="Submit" />
+                <button type='button' className="btnStyle mr-10" onClick={onCancel}>Cancel</button>
+
+            </div>
+        </form>
         </div>
     );
 };
