60c1ea878b3d18c359fa5e219891f64fe7e44aba
vivek-metricdust <vivek.v@metricdust.com>
2024-04-04 19:11:17 +0530

    merge fix

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../utils/auth";
-import "./Login.scss";
+import "./login.scss";
 import logo from "../../assets/orca.png";
 import secureLocalStorage from "react-secure-storage";
 
