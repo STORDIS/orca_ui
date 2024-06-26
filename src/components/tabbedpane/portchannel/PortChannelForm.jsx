38e171539c23cc2a5674eee6427c089d29a756f5
vivek-metricdust <vivek.v@metricdust.com>
2024-06-26 14:36:33 +0000

    changes done

--- src/components/tabbedpane/portchannel/PortChannelForm.jsx
+++ src/components/tabbedpane/portchannel/PortChannelForm.jsx
@@ -15,6 +15,9 @@ const PortChannelForm = ({
 
     const instance = interceptor();
 
+    const [selectedInterfaces, setSelectedInterfaces] = useState([]);
+    const [interfaceNames, setInterfaceNames] = useState([]);
+
     const [formData, setFormData] = useState({
         mgt_ip: selectedDeviceIp || "",
         lag_name: "",
@@ -39,11 +42,22 @@ const PortChannelForm = ({
         onSubmit(formData);
     };
 
-    const handleDropdownChange = (e) => {
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            members: e,
-        }));
+    const handleDropdownChange = (event) => {
+        setSelectedInterfaces((prev) => {
+            const newValue = event.target.value;
+            if (!prev.includes(newValue)) {
+                return [...prev, newValue];
+            }
+            return prev;
+        });
+    };
+
+    const handleRemove = (key) => {
+        setSelectedInterfaces((prev) => {
+            return prev.filter((item) => item !== key);
+        });
+
+        setDisableConfig(false);
     };
 
     const handleValue = (e) => {
@@ -55,15 +69,13 @@ const PortChannelForm = ({
         }
     };
 
-    const [interfaceNames, setInterfaceNames] = useState([]);
-
     useEffect(() => {
         instance
             .get(getAllInterfacesOfDeviceURL(selectedDeviceIp))
             .then((response) => {
-                const fetchedInterfaceNames = response.data.map(
-                    (item) => item.name
-                );
+                const fetchedInterfaceNames = response.data
+                    .map((item) => item.name)
+                    .filter((item) => item.includes("Ethernet"));
                 setInterfaceNames(fetchedInterfaceNames);
             })
             .catch((error) => {
@@ -72,19 +84,19 @@ const PortChannelForm = ({
     }, []);
 
     return (
-        <div className="">
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    handleSubmit(formData);
-                }}
-                className="port-channel-form"
-            >
-                <div className="form-field">
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit(formData);
+            }}
+            className=""
+        >
+            <div className="form-wrapper">
+                <div className="form-field w-50">
                     <label>Device IP:</label>
-                    <span>{selectedDeviceIp}</span>
+                    <input type="text" value={selectedDeviceIp} disabled />
                 </div>
-                <div className="form-field">
+                <div className="form-field w-50">
                     <label htmlFor="lag-name">Channel Name:</label>
                     <input
                         type="text"
@@ -94,7 +106,10 @@ const PortChannelForm = ({
                         onBlur={handleValue}
                     />
                 </div>
-                <div className="form-field">
+            </div>
+
+            <div className="form-wrapper">
+                <div className="form-field w-50">
                     <label>Admin Status:</label>
                     <select
                         name="admin_sts"
@@ -105,7 +120,7 @@ const PortChannelForm = ({
                         <option value="down">down</option>
                     </select>
                 </div>
-                <div className="form-field">
+                <div className="form-field w-50">
                     <label>MTU:</label>
                     <input
                         type="number"
@@ -114,22 +129,18 @@ const PortChannelForm = ({
                         onChange={handleChange}
                     />
                 </div>
+            </div>
 
-                <div className="form-field">
+            <div className="form-wrapper">
+                <div className="form-field w-75">
                     <label>Members:</label>
                     <select
-                        multiple
-                        size="5"
-                        style={{ width: "100%" }}
-                        onChange={(e) =>
-                            handleDropdownChange(
-                                Array.from(
-                                    e.target.selectedOptions,
-                                    (option) => option.value
-                                )
-                            )
-                        }
+                        onChange={handleDropdownChange}
+                        defaultValue={"DEFAULT"}
                     >
+                        <option value="DEFAULT" disabled>
+                            Select Member Interface
+                        </option>
                         {interfaceNames.map((member) => (
                             <option key={member} value={member}>
                                 {member}
@@ -137,24 +148,49 @@ const PortChannelForm = ({
                         ))}
                     </select>
                 </div>
-                <div className="">
-                    <button
-                        type="submit"
-                        className="btnStyle mr-10"
-                        disabled={disableConfig}
-                    >
-                        Apply Config
-                    </button>
-                    <button
-                        type="button"
-                        className="btnStyle mr-10"
-                        onClick={onCancel}
-                    >
-                        Cancel
-                    </button>
+                <div className="form-field mt-25">
+                    {Object.keys(selectedInterfaces).length} selected
                 </div>
-            </form>
-        </div>
+            </div>
+
+            <div className="selected-interface-wrap mb-10 w-100">
+                {Object.entries(selectedInterfaces).map(
+                    ([key, value], index) => (
+                        <div className="selected-interface-list mb-10">
+                            <div key={key} className="ml-10 w-50">
+                                {index + 1} &nbsp; {value}
+                            </div>
+                            <div className=" w-50">
+                                <button
+                                    className="btnStyle ml-25"
+                                    disabled={disableConfig}
+                                    onClick={() => handleRemove(value)}
+                                >
+                                    Remove
+                                </button>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
+
+            <div className="">
+                <button
+                    type="submit"
+                    className="btnStyle mr-10"
+                    disabled={disableConfig}
+                >
+                    Apply Config
+                </button>
+                <button
+                    type="button"
+                    className="btnStyle mr-10"
+                    onClick={onCancel}
+                >
+                    Cancel
+                </button>
+            </div>
+        </form>
     );
 };
 
