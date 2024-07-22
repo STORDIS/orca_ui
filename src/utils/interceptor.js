0a8fc2aa60c6f6541f211ae21d9916c51c2f2948
vivek-metricdust <vivek.v@metricdust.com>
2024-07-22 09:56:24 +0000

    removed time out

--- src/utils/interceptor.js
+++ src/utils/interceptor.js
@@ -6,7 +6,7 @@ const interceptor = () => {
         headers: {
             "Content-Type": "application/json",
         },
-        timeout: 20000, // 20 seconds.
+        // timeout: 20000, // 20 seconds.
     });
 
     instance.interceptors.request.use(
