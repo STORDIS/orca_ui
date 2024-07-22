21d665246afcaa751c28a0a63ab8a0642f1039ce
vivek-metricdust <vivek.v@metricdust.com>
2024-07-22 09:54:45 +0000

    connection time out error message in interseptor

--- src/utils/interceptor.js
+++ src/utils/interceptor.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import secureLocalStorage from "react-secure-storage";
 
 const interceptor = () => {
-
     const instance = axios.create({
         headers: {
             "Content-Type": "application/json",
         },
+        timeout: 20000,
     });
 
     instance.interceptors.request.use(
@@ -16,9 +16,20 @@ const interceptor = () => {
                 config.headers.Authorization = "Token " + token;
             }
             return config;
-            
         },
         (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    instance.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if (error.code === "ERR_NETWORK") {
+                alert("Connection timed out. Please try again.");
+            }
             return Promise.reject(error);
         }
     );
