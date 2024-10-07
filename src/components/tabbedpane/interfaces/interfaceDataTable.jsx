23bf6f27759830ebb2a51ea2af54604c1bf013b0
vivek-metricdust <vivek.v@metricdust.com>
2024-10-07 12:51:23 +0530

    label change

--- src/components/tabbedpane/interfaces/interfaceDataTable.jsx
+++ src/components/tabbedpane/interfaces/interfaceDataTable.jsx
@@ -371,7 +371,7 @@ const InterfaceDataTable = (props) => {
                 <Modal
                     show={true}
                     onClose={relode}
-                    title="Select Sub Interfaces"
+                    title="Interface IP Address"
                     onSubmit={(e) => sendUpdates(e)}
                 >
                     <PrimarySecondaryForm
