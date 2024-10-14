0abe96dbe8f1d8d2b257e732e7982cf6b37c2330
vivek-metricdust <vivek.v@metricdust.com>
2024-10-14 18:16:40 +0530

    description remove fix

--- src/components/tabbedpane/interfaces/interfaceDataTable.jsx
+++ src/components/tabbedpane/interfaces/interfaceDataTable.jsx
@@ -116,8 +116,6 @@ const InterfaceDataTable = (props) => {
     }, []);
 
     const handleCellValueChanged = useCallback((params) => {
-      
-
         if (params.newValue !== params.oldValue) {
             setChanges((prev) => {
                 let latestChanges;
@@ -157,6 +155,16 @@ const InterfaceDataTable = (props) => {
                                 mgt_ip: selectedDeviceIp,
                                 alias: params.data.alias,
                                 [params.colDef.field]: params.newValue,
+                            },
+                        ];
+                    } else if (params.colDef.field === "description") {
+                        latestChanges = [
+                            ...prev,
+                            {
+                                name: params.data.name,
+                                mgt_ip: selectedDeviceIp,
+
+                                [params.colDef.field]: params.newValue || "",
                             },
                         ];
                     } else {
