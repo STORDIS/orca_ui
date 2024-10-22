150584234291dcd7ac6a817fa061dda4e9c1a0d2
vivek-metricdust <vivek.v@metricdust.com>
2024-10-22 10:35:57 +0000

    multiple page added

--- src/pages/ZTPnDHCP/ztpndhcp.jsx
+++ src/pages/ZTPnDHCP/ztpndhcp.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
-
 import Editor from "@monaco-editor/react";
+import "./ztpndhcp.scss";
 
 export const ZTPnDHCP = () => {
     const parentDivRef = useRef(null);
@@ -13,6 +13,28 @@ export const ZTPnDHCP = () => {
         height: "60vh",
         width: "100%",
     });
+
+    const [fileName, setFileName] = useState("script.js");
+
+    const files = {
+        "script.js": {
+            name: "script.js",
+            language: "javascript",
+            value: "console.log('Hello World')",
+        },
+        "style.css": {
+            name: "style.css",
+            language: "css",
+            value: "body { color: red; }",
+        },
+        "index.html": {
+            name: "index.html",
+            language: "html",
+            value: "<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>",
+        },
+    };
+
+    const file = files[fileName];
 
     useEffect(() => {
         window.addEventListener("resize", () => {
@@ -36,14 +58,36 @@ export const ZTPnDHCP = () => {
                 className="resizable"
                 onMouseMove={handleResize}
             >
-                <Editor
-                    height={layout.height}
-                    width={layout.width}
-                    defaultLanguage="json"
-                    defaultValue={code}
-                    path={'../graphsNcharts/graph/CustomGraph.jsx'}
-                    theme="light" // light / vs-dark
-                />
+                <div >
+                    <button
+                        disabled={fileName === "script.js"}
+                        onClick={() => setFileName("script.js")}
+                    >
+                        script.js
+                    </button>
+                    <button
+                        disabled={fileName === "style.css"}
+                        onClick={() => setFileName("style.css")}
+                    >
+                        style.css
+                    </button>
+                    <button
+                        disabled={fileName === "index.html"}
+                        onClick={() => setFileName("index.html")}
+                    >
+                        index.html
+                    </button>
+                    <Editor
+                        height={layout.height}
+                        width={layout.width}
+                        // defaultLanguage="json"
+                        // defaultValue={code}
+                        path={file.name}
+                        defaultLanguage={file.language}
+                        defaultValue={file.value}
+                        theme="light" // light / vs-dark
+                    />
+                </div>
             </div>
         </div>
     );
