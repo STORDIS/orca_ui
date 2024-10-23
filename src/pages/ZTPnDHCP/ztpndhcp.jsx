d6dd65edf51d09fc05cd56e9da77cba248c7cc10
vivek-metricdust <vivek.v@metricdust.com>
2024-10-23 10:53:53 +0000

    tab added

--- src/pages/ZTPnDHCP/ztpndhcp.jsx
+++ src/pages/ZTPnDHCP/ztpndhcp.jsx
@@ -2,13 +2,11 @@ import React, { useRef, useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
 import "./ztpndhcp.scss";
 import { FaRegCircleXmark } from "react-icons/fa6";
+// import { FaCircle } from "react-icons/fa6";
+import { FaCircle } from "react-icons/fa";
 
 export const ZTPnDHCP = () => {
     const parentDivRef = useRef(null);
-
-    const [code, setCode] = useState(
-        `{"key": "value","key2": "value2","key3": "value3"}`
-    );
 
     const [layout, setLayout] = useState({
         height: "60vh",
@@ -20,12 +18,14 @@ export const ZTPnDHCP = () => {
             name: "json1",
             language: "json",
             value: `{"id": "1","name": "Apple","color": "red"}`,
+            status: "saved",
         },
 
         {
             name: "json2",
             language: "json",
             value: `{"id": "2","name": "Banana","color": "yellow"}`,
+            status: "saved",
         },
 
         {
@@ -36,10 +36,16 @@ id: 2
 name: Orange 
 color: orange 
 ---`,
+            status: "saved",
         },
     ];
 
-    const [file, setFile] = useState(files[0]);
+    const [file, setFile] = useState({
+        name: "default",
+        language: "txt",
+        value: "Select a file",
+        status: "saved",
+    });
 
     useEffect(() => {
         handleResize();
@@ -57,11 +63,10 @@ color: orange
         }
     };
 
-    const [tab, setTab] = useState([files[0]]);
+    const [tab, setTab] = useState([]);
 
     const addTab = (list, clickType) => {
         if (clickType === "single") {
-            console.log(clickType);
             const exists = tab.some((item) => item.name === list.name); // Check if the item with the same ID exists
             if (!exists) {
                 setTab([...tab, list]); // Add the item if it doesn't already exist
@@ -73,22 +78,98 @@ color: orange
         setFile(list);
     };
 
+    const remove = (list) => {
+        // setTab(tab.filter((item) => item.name !== list.name));
+
+        if (list.status === "saved") {
+            const updatedTab = tab.filter((item) => item.name !== list.name);
+
+            if (updatedTab.length === 0) {
+                setTab([]);
+                setFile({
+                    name: "default",
+                    language: "txt",
+                    value: "Select a file",
+                });
+            } else {
+                setTab(updatedTab);
+                setFile(updatedTab[0]);
+            }
+        } else {
+            alert("unsaved changes");
+        }
+    };
+
+    const editorChange = (e, fileName) => {
+        const updatedTab = tab.map((item) => {
+            if (item.name === fileName) {
+                return {
+                    ...item,
+                    value: e,
+                    status: "unsaved",
+                };
+            }
+            return item;
+        });
+        // setIsSaved(false);
+        setTab(updatedTab);
+    };
+
+    const save = (list) => {
+        const updatedTab = tab.map((item) => {
+            if (item.name === list.name) {
+                return {
+                    ...item,
+                    status: "saved",
+                };
+            }
+            return item;
+        });
+        // setIsSaved(true);
+
+        setTab(updatedTab);
+    };
+
     return (
         <div className="listContainer">
+            <div className="pl-10 ">
+                <div
+                    className="form-wrapper"
+                    style={{
+                        alignItems: "center",
+                    }}
+                >
+                    <div className="form-field w-25">
+                        <label htmlFor="">DHCP server credentials :</label>
+                    </div>
+                    <div className="form-field w-75">
+                        <input type="text" placeholder="" />
+                    </div>
+                </div>
+            </div>
+
             <div className="editorContainer">
                 <div className="fileSelection">
                     {files.map((list, index) => (
                         <div
                             className={`fileItem ${
-                                file.name === list.name ? "active" : ""
+                                file?.name === list?.name ? "active" : ""
                             }`}
                             key={index}
                             onClick={() => addTab(list, "single")}
                             onDoubleClick={() => addTab(list, "double")}
                         >
-                            {list.name}
+                            {list?.name}
                         </div>
                     ))}
+                    <div className="bottomSection">
+                        <button
+                            className="addFileButton"
+                            onClick={() => save(file)}
+                        >
+                            Save
+                        </button>
+                    </div>
                 </div>
 
                 <div className="editor">
@@ -98,17 +179,31 @@ color: orange
                                 className={`tabButton ${
                                     file.name === list.name ? "tabActive" : ""
                                 }`}
-                                onClick={() => setFile(list)}
                             >
-                                <FaRegCircleXmark
-                                    className="cancel mr-5"
-                                    onClick={() => {
-                                        setTab(
-                                            tab.filter((_, i) => i !== index)
-                                        );
-                                    }}
-                                />
-                                <span className="tabName">{list.name}</span>
+                                <div className="tabName">
+                                    <FaRegCircleXmark
+                                        className="cancel mr-5"
+                                        onClick={() => {
+                                            remove(list);
+                                        }}
+                                    />
+                                    <div
+                                        onClick={() => {
+                                            setFile(list);
+                                        }}
+                                    >
+                                        {list.name}
+                                    </div>
+                                </div>
+                                {list.status === "unsaved" && (
+                                    <div
+                                        onClick={() => {
+                                            setFile(list);
+                                        }}
+                                    >
+                                        <FaCircle />
+                                    </div>
+                                )}
                             </div>
                         ))}
                     </div>
@@ -122,9 +217,12 @@ color: orange
                             height={layout.height}
                             width={layout.width}
                             path={file.name}
-                            defaultLanguage={file.language}
-                            defaultValue={file.value}
+                            language={file.language}
+                            value={file.value}
+                            defaultLanguage={"txt"}
+                            defaultValue={"Select a file"}
                             theme="light" // light / vs-dark
+                            onChange={(e) => editorChange(e, file.name)}
                         />
                     </div>
                 </div>
