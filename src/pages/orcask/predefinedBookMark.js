2fde1641ff08d10c636cee776ad1acdfb6319a44
vivek-metricdust <vivek.v@metricdust.com>
2025-01-09 14:52:51 +0530

    predefined fix

--- src/pages/orcask/predefinedBookMark.js
+++ src/pages/orcask/predefinedBookMark.js
@@ -3,7 +3,7 @@ export const predefinedBookMarks = [
     prompt: "get all device details",
   },
   {
-    prompt: "get \t\t device details",
+    prompt: "get device details",
   },
   {
     prompt: "get vlans for ip \t\t",
